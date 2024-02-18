2669d2463a21a20ee21ed3372f1c2725a6a4295c
Cedric Baaklini <cabaaklini@icloud.com>
2024-02-18 02:05:40 -0500

    Creating the Backend.

--- backend/src/main/kotlin/com/example/backend/HelloWorldController.kt
+++ backend/src/main/kotlin/com/example/backend/HelloWorldController.kt
@@ -1,8 +1,12 @@
-import org.springframework.web.servlet.FilterRegistrationBean
+package com.example.backend
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.core.Ordered
-import org.springframework.web.bind.annotation.*
-import org.springframework.web.cors.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
 import java.util.*
 import javax.servlet.Filter
@@ -15,11 +19,11 @@ class HelloWorldController {
 	}
 
 	@Bean
-	fun simpleCorsFilter(): FilterRegistration<*> {
+	fun simpleCoreFilter(): FilterRegistrationBean<*> {
 		val source = UrlBasedCorsConfigurationSource()
 		val config = CorsConfiguration()
 		config.allowCredentials = true
-		config.allowedOrigins = Collections.singletonList("http://localhost:8080")
+		config.allowedOrigins = Collections.singletonList(("http://localhost:8080"))
 		config.allowedMethods = Collections.singletonList("*")
 		config.allowedHeaders = Collections.singletonList("*")
 		source.registerCorsConfiguration("/**", config)
